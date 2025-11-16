a69cdc290d7a756379a5f6784b45ac7491af3635
wjb <95374542+jbradley2003@users.noreply.github.com>
2025-11-16 13:15:41 -0600

    SAW -> Graph
    At this point, it is possible to generate all conformations for some length n and store them as graphs with each monomer represented as a node

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
  
-# BEAD CLASS
+# Protein Monomer Class
 
 class Node:
-    def __init__(self, loc, pol, neigh):
+    def __init__(self, loc, pol, neigh, start):
         self.position = loc
         self.polarity = pol
         self.neighbors = neigh
+        self.first = start
 
     def connect(self, node):
         self.neighbors.add(node)
+        node.neighbors.add(self)
 
     def isConnected(self, node):
         return node in self.neighbors
     
     def samePolarity(self, node):
-        return self.polarity == node.polarity    
+        return self.polarity == node.polarity
+
+    def isStart(self):
+        return self.first == 1
+
     
-# TESTING CLASS
-a = Node((0,0), 'H', set())
-b = Node((0,0), 'H', {a})
-a.connect(b)
-print(b.samePolarity(a))
+# Testing
+# a = Node((0,0), 'H', set(),0)
+# b = Node((0,0), 'H', {a},1)
+# a.connect(b)
+# print(b.samePolarity(a))
 
 
-# CHAIN GENERATION
+# Chain Generation
 
 # Chain length
 # Directions for a 2D square lattice
@@ -42,10 +48,11 @@ def enumerate_saws(n):
     walk = [start]
     occupied = {start}
 
+
     def dfs():
         # If we've reached the desired chain length, yield the walk
         if len(walk) == n:
-            yield list(walk)  # return a copy
+            yield walk   # return a copy
             return
 
         # Try extending in all directions
@@ -69,5 +76,28 @@ def enumerate_saws(n):
 
     yield from dfs()
 
+# Turn SAW into graph
 
+def sawToGraph(saw):
+    g = []
+    i = 0
+    for b in saw:
+        if i == 0:
+            # Node(loc, polarity, neighbors_set, start_flag)
+            g.append(Node(b, 'H', set(), 1))
+        else:
+            g.append(Node(b, 'H', set(), 0))
+            g[i].connect(g[i-1])
+        i += 1
+    return g
 
+g = []
+for w in enumerate_saws(3):
+    g.append(sawToGraph(w))
+
+for gr in g:
+    print('new graph')
+    for n in gr:
+        print(n.position)
+        print()
+        print(n.isStart())
